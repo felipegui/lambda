6215802d4d6ac93ed7a6d6f508c1cfb73abd645e
Felipe Guimaraes <fguiengenharia@gmail.com>
2025-05-31 18:57:13 -0300

    Estudo de caso 4: Otimizando a expressao lambda

--- Lambda/src/application/Program.java
+++ Lambda/src/application/Program.java
@@ -14,9 +14,7 @@ public class Program {
         list.add(new Product("TV", 900.00));
         list.add(new Product("Notebook", 1200.00));
 
-        Comparator<Product> comp = (prod1, prod2) -> {
-            return prod1.getName().toUpperCase().compareTo(prod2.getName().toUpperCase());
-        };
+        Comparator<Product> comp = (prod1, prod2) -> prod1.getName().toUpperCase().compareTo(prod2.getName().toUpperCase());
 
         list.sort(comp);
 
